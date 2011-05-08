c33169545c092eec65a3a56db0dfb638b48e430c
Randall Pruim <rpruim@calvin.edu>
2011-05-08 05:46:25 +0000

    version 0.2-14

--- R/xhistogram.R
+++
@@ -1,7 +0,0 @@
-xhistogram <-
-function (..., type = "density", labels = F, density = FALSE, 
-    fit = NULL, start = NULL) 
-{
-    histogram(..., panel = panel.xhistogram, type = type, fit = fit, 
-        start = start, labels = labels, density = density)
-}
